41ee21edb8b25d8df86977f1cb365c12448d737d
Garrett Hoofman <gambitsunob@gmail.com>
2014-12-23 18:39:11 -0500

    Initial Template

---
+++ CMake/engine/opifex.cmake
@@ -0,0 +1,9 @@
+
+### CMake Setup
+include(CMake/engine/components.cmake) 
+include(CMake/engine/macros.cmake) 
+### Build Setup
+include(CMake/engine/build.cmake)
+include(CMake/engine/global.cmake)
+include(CMake/engine/packages.cmake)
+include(CMake/assets.cmake)
---
+++ src/Main.cpp
@@ -0,0 +1,50 @@
+//////////////////////////////////////
+// Application Entry Point
+//////////////////////////////////////
+#include "./Engine.h"
+
+//////////////////////////////////////
+// Application Methods
+//////////////////////////////////////
+
+void ApplicationInit() {
+	OPchar* assetDir = NULL;
+#ifdef OPIFEX_REPO
+	assetDir = OPIFEX_ASSETS;
+#endif
+	OPloadersAddDefault();
+	OPcmanInit(assetDir);
+
+	OPrenderInit();
+}
+
+int ApplicationUpdate(OPtimer* timer) {
+	OPrenderClear(0,0,0);
+	OPrenderPresent();
+	return 0;
+}
+
+void ApplicationDestroy() {
+
+}
+
+void ApplicationSetup() {
+	OPinitialize = ApplicationInit;
+	OPupdate = ApplicationUpdate;
+	OPdestroy = ApplicationDestroy;
+}
+
+//////////////////////////////////////
+// Application Entry Point
+//////////////////////////////////////
+OP_MAIN {
+	OPlog("Starting up OPifex Engine");
+
+	OPinitialize = ApplicationInit;
+	OPupdate = ApplicationUpdate;
+	OPdestroy = ApplicationDestroy;
+
+	OP_MAIN_START
+	OP_MAIN_END
+	OP_MAIN_SUCCESS
+}
